965f89d4c68497a5112723d1c8125bc5d6644d79
admin <admin@DESKTOP-KK7KSL7>
2025-03-14 17:48:18 +0900

    숙제

--- HelloWorld/src/com/yedam/bookApp/BookApp.java
+++ HelloWorld/src/com/yedam/bookApp/BookApp.java
@@ -13,5 +13,12 @@ public class BookApp {
 		
 		mainApp2.list();
 		
+		// 숙제:
+				// 1번) BookMain의 main메소드를 통해서만 기능활용하도록 하세요.
+				// User 클래스를 생성하고
+				// BookMain에 User[]을 선언해서 회원을 3명 등록하기.
+				// BookMain에 login메소드를 선언하고 매개값으로 아이디와 비밀번호를 입력받도록 한다.
+				// login메소드는 User[]에 등록된 회원중에서 입력받은 아이디와 비밀번호가 있으면 로그인성공 아니면 실패.
+				// login성공 했을 경우에만 1번) main메소드를 실행하도록 한다
 	}
 }
---
+++ HelloWorld/src/com/yedam/bookApp/User.java
@@ -0,0 +1,13 @@
+package com.yedam.bookApp;
+
+public class User {
+	private String userId;
+	private String userName;
+	private String password;
+
+	public User(String userId, String userName, String password) {
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+	}
+}
